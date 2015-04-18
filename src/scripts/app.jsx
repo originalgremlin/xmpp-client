dc6f089794a46f201948ca81dcd84721c3c7b341
Barry Shapira <barry@aerofs.com>
2015-04-17 17:16:56 -0700

    Basic settings with real routing.

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -4,45 +4,61 @@
     var fs = require('fs'),
         path = require('path'),
         React = require('react'),
-        FileExplorer = require('./build/scripts/components/file-explorer.js'),
         ChatClient = require('./build/scripts/components/chat-client.js'),
+        FileExplorer = require('./build/scripts/components/file-explorer.js'),
+        Settings = require('./build/scripts/components/settings.js'),
         i18n = require('./build/scripts/util/i18n.js'),
         _ = require('lodash');
     require('./build/scripts/util/devtools.js');
     require('./build/scripts/util/atom-watcher.js');
     window.i18n = i18n;
 
-    var home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
-        aero = path.join(home, 'AeroFS'),
-        root = fs.existsSync(aero) ? aero : home;
+    var Router = require('react-router'),
+        DefaultRoute = Router.DefaultRoute,
+        Link = Router.Link,
+        Route = Router.Route,
+        RouteHandler = Router.RouteHandler;
 
     var App = React.createClass({
         render: function() {
             return (
-                <div id="app">
-                    <div id="file-explorer">
-                        <FileExplorer refs="fileExplorer" root={ root } />
-                    </div>
-                    <div id="chat-client">
-                        <ChatClient ref="chatClient" />
-                    </div>
-                    <RouteHandler />
+                <div>
+                    <header>
+                        <ul>
+                            <li><Link to="files">Files</Link></li>
+                            <li><Link to="chat">Chat</Link></li>
+                            <li><Link to="settings">Settings</Link></li>
+                        </ul>
+                    </header>
+                    <RouteHandler/>
                 </div>
             );
         }
     });
 
-    var Router = require('react-router'),
-        Route = Router.Route,
-        RouteHandler = Router.RouteHandler,
-        DefaultRoute = Router.DefaultRoute;
-
     var routes = (
-        <Route name="App" handler={ App } path={ __filename }>
+        <Route name="app" handler={ App } path={ __filename }>
+            <Route name="files" handler={ FileExplorer } />
+            <Route name="chat" handler={ ChatClient } />
+            <Route name="settings" handler={ Settings } />
+            <DefaultRoute handler={ FileExplorer }/>
         </Route>
     );
 
     Router.run(routes, Router.HistoryLocation, function (Handler) {
         React.render(<Handler />, document.body);
     });
+
+    /*
+    var home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
+        aero = path.join(home, 'AeroFS'),
+        root = fs.existsSync(aero) ? aero : home;
+
+    <div id="file-explorer">
+        <FileExplorer refs="fileExplorer" root={ root } />
+    </div>
+    <div id="chat-client">
+        <ChatClient ref="chatClient" />
+    </div>
+    */
 })();
