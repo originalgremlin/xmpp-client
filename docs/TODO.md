3a2051b397a339500ac24272083087efe2f47a89
Barry Shapira <barry@aerofs.com>
2015-04-20 11:53:45 -0700

    Internationalized main process.

--- docs/TODO.md
+++ docs/TODO.md
@@ -6,7 +6,6 @@
 * fix problems with dyld.  common with many versions of node.  not on my personal laptop, though
     dyld: lazy symbol binding failed: Symbol not found: _node_module_register
 * kill Atom.app, node-inspector on script "stop"
-* can i18n work in main process? how do we detect user language?
 * Settings (localStorage does persist)
 * Login/logout logic
 * highlight matched text in searches to give context
--- src/scripts/main/tray.jsx
+++ src/scripts/main/tray.jsx
@@ -1,7 +1,8 @@
 (function(undefined) {
     'use strict';
 
-    var Menu = require('menu'),
+    var i18n = require('../util/i18n.js'),
+        Menu = require('menu'),
         Tray = require('tray');
 
     module.exports = {
@@ -9,10 +10,10 @@
             var tray = new Tray('./src/images/main/tray.png');
             tray.setToolTip('This is my application.');
             tray.setContextMenu(Menu.buildFromTemplate([
-                {label: 'Item1', type: 'radio'},
-                {label: 'Item2', type: 'radio'},
-                {label: 'Item3', type: 'radio', checked: true},
-                {label: 'Item4', type: 'radio'}
+                {label: i18n.t('Item1'), type: 'radio'},
+                {label: i18n.t('Item2'), type: 'radio'},
+                {label: i18n.t('Item3'), type: 'radio', checked: true},
+                {label: i18n.t('Item4'), type: 'radio'}
             ]));
         }
     };
